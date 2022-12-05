79230b506eac2f38a85ac2228d967da9ad14958b
Ghost116rus <danilkaleev@mai.ru>
2022-12-05 15:11:58 +0300

    Основные классы созданы, реализовано стартовое окно, теперь нужно приступать к отдельным реализация по функциям

---
+++ TransportCo/MyHttp/MyHttpClient.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportCo.MyHttp
+{
+    public static class MyHttpClient
+    {
+        static int currentIdUser;
+
+        #region АВТОРИЗАЦИЯ
+
+        public static bool Authorizate(string login, string password, ref string error, ref string typeOfAccount)
+        {
+            if (login == "admin" && password == "admin")
+            {
+                typeOfAccount = "Admin";
+                return true;
+            }
+            if (login == "driver" && password == "123")
+            {
+                typeOfAccount = "Driver";
+                return true;
+            }
+            if (login == "Operator" && password == "123")
+            {
+                typeOfAccount = "Operator";
+                return true;
+            }
+            error = "Аккаунт не найден!\n";
+            return false;
+        }
+
+        #endregion
+    }
+}
---
+++ TransportCo/ViewModel/DataManagerAuthorizationVM.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TransportCo.Model;
+using TransportCo.View;
+
+namespace TransportCo.ViewModel
+{
+    public class DataManagerAuthorizationVM : INotifyPropertyChanged
+    {
+        #region Свойства 
+
+        private string? login;
+        public string? Login
+        {
+            get { return login; }
+            set { login = value; NotifyPropertyChanged("Login"); }
+        }
+
+        private string? password;
+        public string? Password
+        {
+            get { return password; }
+            set { password = value; NotifyPropertyChanged("Password"); }
+        }
+
+        private string? errorlog;
+        public string? Errorlog
+        {
+            get { return errorlog; }
+            set { errorlog = value; NotifyPropertyChanged("Errorlog"); }
+        }
+
+        #endregion
+
+        #region Команды
+
+        private RelayCommand authorization;
+        public RelayCommand Authorization
+        {
+            get
+            {
+                return authorization ?? new RelayCommand(obj =>
+                {
+                    Errorlog = "";
+                    if ((login != null && password != null) && (login != "" && password != ""))
+                    {
+                        string? error = null;
+                        string? type = null;
+
+                        if (MyHttp.MyHttpClient.Authorizate(login, password, ref error, ref type))
+                        {
+                            OpenWindow(type); return;
+                        }
+                        else
+                        {
+                            Errorlog = error;
+                        }
+                    }
+                    else
+                    {
+                        Errorlog = "Введите логин и пароль";
+                    }
+                }
+                );
+            }
+        }
+
+        #endregion
+
+        private void OpenWindow(string type)
+        {
+            Window window = null;
+
+            if (type == "Admin")
+            {
+                window = new View.Administrator.AdministratorWindow();
+            }
+            else if (type == "Operator")
+            {
+                window = new View.Administrator.AdministratorWindow();
+            }
+            else
+            {
+                window = new View.Driver.DriverWindow();
+            }
+
+            window.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            window.Show();
+            StartWindow._window.Close();
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

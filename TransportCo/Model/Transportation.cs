098334f8a1a5780f5f5b399e987f6c8d63bb6e0d
Ghost116rus <danilkaleev@mai.ru>
2022-12-12 03:14:21 +0300

    Пока всё

--- TransportCo/Model/Transportation.cs
+++ TransportCo/Model/Transportation.cs
@@ -1,10 +1,20 @@
-﻿namespace TransportCo.Model
+﻿using System;
+
+namespace TransportCo.Model
 {
     public class Transportation
     {
         public int Number { get; set; }
-        public string Addres { get; set; }
-        public string FullName { get; set; }
+        public int Num_Sending_storage { get; set; }
+        public string Sending_storage_Addres { get; set; }
+        public DateTime Date_dispatch { get; set; }
+        public DateTime Delivery_date { get; set; }
+        public int Total_time { get; set; }
+        public int Total_length { get; set; }
+        public int Car_load { get; set; }
+        public int Total_shipping_cost { get; set; }
         public string Status { get; set; }
+        public Transport_vehicle vehicle { get; set; }
+        public Driver driver { get; set; }
     }
 }

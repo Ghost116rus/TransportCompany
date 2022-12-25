3ae04c1e971e664d1dbc78230009294a8fd19159
Ghost116rus <danilkaleev@mai.ru>
2022-12-25 14:20:43 +0300

    Создал три страницы

---
+++ TransportCo/Model/OperatorDetailTransportaion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportCo.Model
+{
+    public class OperatorDetailTransportaion
+    {
+        public int Number { get; set; }
+        public DateTime Date_dispatch { get; set; }
+        public string Status { get; set; }
+
+        public string Vehicle_identification_number { get; set; }
+        public string Name { get; set; }
+
+        public string FullName { get; set; }
+        public string Phone_number { get; set; }
+
+        public List<Product> Requare_Products { get; set; } = null;
+    }
+}
---
+++ TransportCo/View/Operator/Pages/Transportation/TransportationsPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace TransportCo.View.Operator.Pages.Transportation
+{
+    /// <summary>
+    /// Логика взаимодействия для TransportationsPage.xaml
+    /// </summary>
+    public partial class TransportationsPage : Page
+    {
+        public static Frame _transportationDetailFrame { get; set; }
+        public static Page _GetTransportationPage { get; set; }
+        public static Page _SendTransportationPage { get; set; }
+        public TransportationsPage()
+        {
+            InitializeComponent();
+            DataContext = OperatorWindow._mng;
+            _transportationDetailFrame = this.TransportationDetail;
+            _GetTransportationPage = new GetTransportationPage();
+            _SendTransportationPage = new SendTransportationPage();
+        }
+    }
+}
